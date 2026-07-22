89c9e748f562bdd6797d0d4a413e391d0bc64d3f
Viktoria Karpenko <ocenkachprao2003@gmail.com>
2026-07-22 14:11:18 +0300

    nodejs-hw-4 completed

--- src/models/note.js
+++ src/models/note.js
@@ -19,7 +19,11 @@ const noteSchema = new Schema(
       required: false,
       enum: TAGS,
       default: 'Todo',
-      index: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
   },
   {
@@ -27,5 +31,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ userId: 1, tag: 1 });
 
 export const Note = model('Note', noteSchema);
